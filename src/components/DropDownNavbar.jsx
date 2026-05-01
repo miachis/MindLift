abe663df09c5421b07fe1ded1fe5ee82e67ca58b
Miachi Solomon <smiachi10@gmail.com>
2026-05-01 13:16:43 +0100

    fixing mobile navbar

--- src/components/DropDownNavbar.jsx
+++ src/components/DropDownNavbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function DropDownNavbar() {
   return (
-    <div className="fixed inset-0 top-full z-200 flex flex-col gap-50 px-5 py-3 w-full h-full bg-green-300">
+    <div className="flex flex-col justify-between px-1 pt-20 pb-10 w-full h-screen fixed inset-0 bg-white">
       <nav className="px-8 py-2">
         <div className="border-b border-gray-300 font-medium text-md py-4">
           <a href="#about">About</a>
@@ -15,11 +15,11 @@ function DropDownNavbar() {
         </div>
       </nav>
 
-      <div className="flex flex-col gap-2 px-8 py-2">
-        <div className="bg-black rounded-full text-white shadow-md w-full text-center p-2 font-bold text-sm">
+      <div className="flex flex-col gap-2 px-6 py-3 mt-60">
+        <div className="bg-black rounded-full text-white shadow-md w-full text-center p-3 font-semibold text-md">
           <Link to={"/signup"}>Get started</Link>
         </div>
-        <div className="border-gray-300 border rounded-full w-full text-center p-1">
+        <div className="border-gray-300 border rounded-full w-full text-center p-3 text-md">
           <Link to={"/login"}>Log in</Link>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,78 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DropDownNavbar from "./DropDownNavbar";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
   return (
-    <header className="sticky top-0 z-100 px-10 flex justify-center items-center">
-      <div className="w-full flex border justify-between items-center shadow-sm border-white rounded-full backdrop-blur-md bg-white/30 py-2 pl-4 pr-5 mt-3 lg:py-2.5 lg:pl-8 lg:pr-4 2xl:py-5">
-        {open && <DropDownNavbar />}
-        <div className="flex justify-between w-full items-center lg:w-max lg:gap-15">
-          <a className="flex items-center" href="#">
-            <img src="./logo.svg" className="h-6 lg:h-7" />
-            <p className="text-2xl font-bold">MindLift</p>
-          </a>
+    <>
+      {open && <DropDownNavbar />}
+      <header className="sticky top-0 z-100 px-10 flex justify-center items-center">
+        <div className="w-full flex border justify-between items-center shadow-sm border-white rounded-full backdrop-blur-md bg-white/30 py-2 pl-4 pr-5 mt-3 lg:py-2.5 lg:pl-8 lg:pr-4 2xl:py-5">
+          <div className="flex justify-between w-full items-center lg:w-max lg:gap-15">
+            <a className="flex items-center" href="#">
+              <img src="./logo.svg" className="h-6 lg:h-7" />
+              <p className="text-2xl font-bold">MindLift</p>
+            </a>
 
-          <nav className="flex gap-10 text-md">
-            <a
-              href="#about"
-              className="hidden lg:block font-medium text-md hover:text-gray-700"
-            >
-              About
-            </a>
-            <a
-              href="#faq"
-              className="hidden lg:block font-medium text-md hover:text-gray-700"
-            >
-              FAQ
-            </a>
-            <a
-              href="#contact"
-              className="hidden lg:block font-medium text-md hover:text-gray-700"
-            >
-              Contact
-            </a>
-            <button
-              className="cursor-pointer lg:hidden flex items-center justify-center w-10 h-10 rounded-full"
-              onClick={() => setOpen(!open)}
-            >
-              <div className="w-5 h-5 flex flex-col justify-center items-center space-y-1">
-                <span className="block w-5 h-0.5 bg-black"></span>
-                <span className="block w-5 h-0.5 bg-black"></span>
-                <span className="block w-5 h-0.5 bg-black"></span>
-              </div>
-            </button>
-          </nav>
-        </div>
+            <nav className="flex gap-10 text-md">
+              <a
+                href="#about"
+                className="hidden lg:block font-medium text-md hover:text-gray-700"
+              >
+                About
+              </a>
+              <a
+                href="#faq"
+                className="hidden lg:block font-medium text-md hover:text-gray-700"
+              >
+                FAQ
+              </a>
+              <a
+                href="#contact"
+                className="hidden lg:block font-medium text-md hover:text-gray-700"
+              >
+                Contact
+              </a>
+              <button
+                className="cursor-pointer lg:hidden flex items-center justify-center w-10 h-10 rounded-full"
+                onClick={() => setOpen(!open)}
+              >
+                {open ? (
+                  <div class="relative w-5 h-5">
+                    <span class="absolute w-5 h-0.5 bg-black rotate-45 top-1/2 left-0 -translate-y-1/2"></span>
+                    <span class="absolute w-5 h-0.5 bg-black -rotate-45 top-1/2 left-0 -translate-y-1/2"></span>
+                  </div>
+                ) : (
+                  <div className="w-5 h-5 flex flex-col justify-center items-center space-y-1">
+                    <span className="block w-5 h-0.5 bg-black"></span>
+                    <span className="block w-5 h-0.5 bg-black"></span>
+                    <span className="block w-5 h-0.5 bg-black"></span>
+                  </div>
+                )}
+              </button>
+            </nav>
+          </div>
 
-        <div className=" hidden bg-black text-white shadow-sm font-bold px-5 py-2 rounded-full text-sm lg:block">
-          <Link to={"/login"}>Get Started</Link>
+          <div className=" hidden bg-black text-white shadow-sm font-bold px-5 py-2 rounded-full text-sm lg:block">
+            <Link to={"/login"}>Get Started</Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
